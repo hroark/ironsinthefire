c6776fa0475ba39ee00cb4d4b3af691e96a76cb0
hroark <rigamonk@hotmail.com>
2022-07-28 01:39:35 -0500

    Links added

---
+++ src/components/Demo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Demo = () => {
+return (
+	<div>
+	<h1>View Demo</h1>
+	</div>
+);
+};
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 import H from "././App"
 import V from "./Videos"
 //import { send } from 'emailjs-com';
-
+import { BrowserRouter as Router, Switch, 
+    Route, Redirect,} from "react-router-dom";
+    import Home from "./components/Home"; 
+    import Demo from "./components/Demo";
+    
 ReactDOM.render( V, document.getElementById('next'));
 ReactDOM.render( <H />, document.getElementById('root'));
 
